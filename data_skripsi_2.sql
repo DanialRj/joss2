afdcbd207be2247ce2fada737b76de34b069e0a4
danial teemoid <danial.teemoid@gmail.com>
2020-08-10 02:41:15 +0700

    add

--- data_skripsi_2.sql
+++ data_skripsi_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 08, 2020 at 03:36 AM
+-- Generation Time: Aug 09, 2020 at 09:40 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -32,19 +32,16 @@ CREATE TABLE `admin` (
   `id_admin` int(11) NOT NULL,
   `nama_admin` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id_admin`, `nama_admin`, `username`, `password`, `created_by`, `created_at`, `updated_by`, `updated_at`) VALUES
-(2, 'admin', 'admin@admin.com', 'admin123', 0, '0000-00-00', 0, '0000-00-00');
+INSERT INTO `admin` (`id_admin`, `nama_admin`, `username`, `password`) VALUES
+(2, 'admin', 'admin@admin.com', 'admin123'),
+(3, 'tahu', 'temo@temo.com', 'ashd');
 
 -- --------------------------------------------------------
 
@@ -56,11 +53,7 @@ CREATE TABLE `kelas` (
   `id_kelas` int(11) NOT NULL,
   `id_wali_kelas` int(11) DEFAULT NULL,
   `kelas` varchar(255) DEFAULT NULL,
-  `jurusan` varchar(255) DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `jurusan` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -73,19 +66,15 @@ CREATE TABLE `lilas` (
   `id_wali_kelas` int(11) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `alert` date DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `alert` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lilas`
 --
 
-INSERT INTO `lilas` (`id_wali_kelas`, `username`, `password`, `alert`, `created_by`, `created_at`, `updated_by`, `updated_at`) VALUES
-(2, 'wali@wali.com', 'wali123', '2020-07-23', 0, '0000-00-00', 0, '0000-00-00');
+INSERT INTO `lilas` (`id_wali_kelas`, `username`, `password`, `alert`) VALUES
+(2, 'wali@wali.com', 'wali123', '2020-07-23');
 
 -- --------------------------------------------------------
 
@@ -99,11 +88,7 @@ CREATE TABLE `nilai_sosial` (
   `catatan_perilaku` text,
   `btr_sikap_spiritual` int(11) DEFAULT NULL,
   `btr_sikap_sosial` int(11) DEFAULT NULL,
-  `nilai_sosial` int(11) DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `nilai_sosial` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -116,11 +101,7 @@ CREATE TABLE `nipel` (
   `id_jenis_pelanggaran` int(11) NOT NULL,
   `id_kategori_pelanggaran` int(11) DEFAULT NULL,
   `jenis_pelanggaran` varchar(255) DEFAULT NULL,
-  `bobot_poin` int(11) DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `bobot_poin` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -137,11 +118,7 @@ CREATE TABLE `pelanggaran` (
   `id_nilai_sosial` int(11) DEFAULT NULL,
   `id_jenis_pelanggaran` int(11) DEFAULT NULL,
   `bobot_poin` int(11) DEFAULT NULL,
-  `at` date DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -156,11 +133,7 @@ CREATE TABLE `rilah` (
   `bobot_dari` int(11) DEFAULT NULL,
   `bobot_sampai` int(11) DEFAULT NULL,
   `sanksi` varchar(255) DEFAULT NULL,
-  `ket` text,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `ket` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -171,11 +144,7 @@ CREATE TABLE `rilah` (
 
 CREATE TABLE `ripel` (
   `id_kategori_pelanggaran` int(11) NOT NULL,
-  `kategori_pelanggaran` varchar(255) DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `kategori_pelanggaran` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -191,11 +160,7 @@ CREATE TABLE `siswa` (
   `nama_siswa` varchar(255) DEFAULT NULL,
   `jenis_kelamin` varchar(255) DEFAULT NULL,
   `alamat` text,
-  `tgl_lahir` date DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `tgl_lahir` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -208,19 +173,15 @@ CREATE TABLE `tim_stp2k` (
   `id_tim` int(11) NOT NULL,
   `nama_tim` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `updated_by` int(11) DEFAULT NULL,
-  `updated_at` date DEFAULT NULL
+  `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tim_stp2k`
 --
 
-INSERT INTO `tim_stp2k` (`id_tim`, `nama_tim`, `username`, `password`, `created_by`, `created_at`, `updated_by`, `updated_at`) VALUES
-(2, 'tim1', 'tim@tim.com', 'tim123', 0, '0000-00-00', 0, '0000-00-00');
+INSERT INTO `tim_stp2k` (`id_tim`, `nama_tim`, `username`, `password`) VALUES
+(2, 'tim1', 'tim@tim.com', 'tim123');
 
 --
 -- Indexes for dumped tables
@@ -295,7 +256,7 @@ ALTER TABLE `tim_stp2k`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `kelas`
@@ -307,7 +268,7 @@ ALTER TABLE `kelas`
 -- AUTO_INCREMENT for table `lilas`
 --
 ALTER TABLE `lilas`
-  MODIFY `id_wali_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_wali_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `nilai_sosial`
@@ -349,7 +310,7 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT for table `tim_stp2k`
 --
 ALTER TABLE `tim_stp2k`
-  MODIFY `id_tim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_tim` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
